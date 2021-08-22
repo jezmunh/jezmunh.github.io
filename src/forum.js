380b815ed03c1684c6da108fd85bb35e1591c986
jezmunh <62074408+jezmunh@users.noreply.github.com>
2021-08-22 16:47:49 +0200

    Update forum.js

--- src/forum.js
+++ src/forum.js
@@ -1,6 +1,6 @@
 async function GetUsers() {
     // отправляет запрос и получаем ответ
-    const response = await fetch("/topic/", {
+    const response = await fetch("/api/users", {
         method: "GET",
         headers: { "Accept": "application/json" }
     });
